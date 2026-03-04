c6643fbfdd1e8968f9491b95290f02ff78ddbb7d
Critchfieldz21 <critchfieldz21@gmail.com>
2026-03-03 19:16:00 -0500

    create the login page for the inventory tracker

---
+++ inventory-tracker/src/login/loginPage.js
@@ -0,0 +1,34 @@
+import logo from '../images/inventory_logo.png';
+import './loginPage.css';
+
+function App() {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <div className="header">
+          <img src={logo} className="login-logo" alt="logo" />
+          <p>Please enter your details to sign in.</p>
+        </div>
+        
+        <form className="login-form">
+          <div className="input-group">
+            <label>Username</label>
+            <input type="text" placeholder="Username" required />
+          </div>
+          
+          <div className="input-group">
+            <label>Password</label>
+            <input type="password" placeholder="Password" required />
+          </div>
+
+
+          <button type="submit" className="login-button">Sign In</button>
+        </form>
+        
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
